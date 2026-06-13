b5d418de034a2441b6e2bcdb3cae38b2d7c4b81c
gwynbleiddrivia <yasinzme2@gmail.com>
2026-06-13 07:52:10 +0600

    urls added, pagination added and rest_framework added as installed_app in settings.py

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -17,6 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+from tutorial.quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r"users", views.UserViewset)
+router.register(r"groups", views.GroupViewSet)
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path("", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework"))
 ]
